90a431478af10d6d5adf25782955ac9f96c70234
2A Overcash <2aovercashn@orangembpr-iloop.lexrich5.org>
2016-09-29 11:32:39 -0400

    s

---
+++ aa/aa/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  aa
+//
+//  Created by 2A Overcash, N on 9/29/16.
+//  Copyright © 2016 smileytechguy. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
---
+++ aa/aa/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  aa
+//
+//  Created by 2A Overcash, N on 9/29/16.
+//  Copyright © 2016 smileytechguy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Do any additional setup after loading the view.
+}
+
+- (void)setRepresentedObject:(id)representedObject {
+    [super setRepresentedObject:representedObject];
+
+    // Update the view, if already loaded.
+}
+
+@end

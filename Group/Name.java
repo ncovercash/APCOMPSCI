750dc96029d837a5bfd5d68d72d8f905c138d95e
2A Overcash <2aovercashn@orangembpr-iloop.lexrich5.org>
2016-10-26 11:36:34 -0400

    Name

--- Group/Name.java
+++ Group/Name.java
@@ -1,5 +1,43 @@
 public class Name {
+	private String lastName, firstName, middleName;
+	
 	public Name() {
-		// init
+		lastName = "last";
+		middleName = "middle";
+		firstName = "first";
+	}
+
+	public Name(String ln, String fn, String mn) {
+		lastName = ln;
+		firstName = fn;
+		middleName = mn;
+	}
+
+	public String toString() {
+		return lastName+", "+firstName+", "+middleName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getMiddleName() {
+		return middleName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setFirstName(String fn) {
+		firstName = fn;
+	}
+
+	public void setMiddleName(String mn) {
+		middleName = mn;
+	}
+
+	public void setLastName(String ln) {
+		lastName = ln;
 	}
 }
--- Group/Tester.java
+++ Group/Tester.java
@@ -1,5 +1,17 @@
 public class Tester {
 	public static void main(String[] args) {
-		// init
+		Name nameTest1, nameTest2, nameTest3;
+		nameTest1 = new Name();
+		nameTest2 = new Name("l", "f", "m");
+		nameTest3 = new Name("l", "f", "m");
+		System.out.println(nameTest1);
+		System.out.println(nameTest2);
+		System.out.println(nameTest3.getLastName());
+		System.out.println(nameTest3.getMiddleName());
+		System.out.println(nameTest3.getLastName());
+		nameTest3.setLastName("a");
+		nameTest3.setFirstName("b");
+		nameTest3.setMiddleName("c");
+		System.out.println(nameTest3);
 	}
 }
